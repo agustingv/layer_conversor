d2c8d4345d2c6fe1b5f3853c6ccbf04737a17351
Agustín García <contacto@agustin-garcia.es>
2026-04-27 11:43:14 +0200

    add new create command user

---
+++ app/api/src/Command/CreateUserCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(name: 'app:create-user', description: 'Create a user')]
+class CreateUserCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $em,
+        private UserPasswordHasherInterface $hasher,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'User email')
+            ->addArgument('password', InputArgument::REQUIRED, 'Plain password')
+            ->addArgument('role', InputArgument::OPTIONAL, 'Role (e.g. ROLE_ADMIN)', 'ROLE_USER');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $user = new User();
+        $user->setEmail($input->getArgument('email'));
+        $user->setRoles([$input->getArgument('role')]);
+        $user->setPassword($this->hasher->hashPassword($user, $input->getArgument('password')));
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $output->writeln(sprintf('Created user <info>%s</info> with role <info>%s</info>', $user->getEmail(), $input->getArgument('role')));
+
+        return Command::SUCCESS;
+    }
+}
